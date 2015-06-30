6e1accfd3b7f4afd076ccb891767871903506ad8
ihgoo <ihgoo2008@gmail.com>
2015-06-30 10:16:17 +0800

    移除方法

--- src/com/ihgoo/allinone/util/Check.java
+++ src/com/ihgoo/allinone/util/Check.java
@@ -26,62 +26,7 @@ public class Check {
             return false;
         }
     }
-    
-    
-    /**
-     * 检查是否连接WIFI
-     *
-     * @return 连接wifi返回true，否则返回false
-     */
-    public static boolean isWifiConnected(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifiNetworkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (wifiNetworkInfo.isConnected()) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * 检查是否连接网络
-     *
-     * @return 连接返回true，否则返回false
-     */
-    public static boolean isNetworkConnected(Context context) {
-        ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
-        if (mNetworkInfo != null) {
-            return mNetworkInfo.isAvailable();
-        }
-        return false;
-    }
-
-    /**
-     * Check all NetworkInfo state which has been connected.
-     * If not find the one is connected, always return false.
-     *
-     * @return
-     */
-    public boolean isNetworkOnline(Context context) {
-        boolean status = false;
-        try {
-            // Check all NetworkInfo state which has been connected.
-            // If not find the one is connected, always return false.
-            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
-            for (int index = 0; index < networkInfos.length; index++) {
-                NetworkInfo netInfo = networkInfos[index];
-                if ((netInfo != null) && netInfo.isConnected()) {
-                    status = true;
-                }
-            }
-        } catch (Exception e) {
-            // Only print the exception message and stack trace,
-            // because this method must return a value to caller.
-            e.printStackTrace();
-        }
-        return status;
-    }
+   
     
     /**
      * 检测App是否在前台
